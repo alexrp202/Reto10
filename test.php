e18037228f76351c607007ad809e9ec637fc2bc7
alexrp202 <72034440+alexrp202@users.noreply.github.com>
2021-05-17 23:07:26 +0200

    Update test.php

--- test.php
+++ test.php
@@ -5,26 +5,15 @@
 
     header('Content-Type: application/json');
 
-    require("db.php"); // IMPORTA EL ARCHIVO CON LA CONEXION A LA DB
-
-    $conexion = conexion(); // CREA LA CONEXION
     
- // REALIZA LA QUERY A LA DB
- $resultado = mysqli_query($conexion, 
- $sql = "SELECT idCalendario, idDieta, idRutina, comentarios,idUsuario,fecha 
- FROM calendario
- WHERE idUsuario=1");
 
- 
 
-   while ($registros = mysqli_fetch_array($resultado)) {
-     $array[$x]=$registros;
-     $x++;
-   }
+    
+		$hola="sannca";
 
 
 
-    echo json_encode($array); // MUESTRA EL JSON GENERADO
+    echo json_encode($hola); // MUESTRA EL JSON GENERADO
 
 ?>
 
